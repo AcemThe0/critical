da31b184d201ac9908e4ba1acc83cd0bcc4f8b6d
AcemThe0 <52658045+AcemThe0@users.noreply.github.com>
2022-11-05 00:39:08 +0200

    Delete src/main directory

--- src/main/java/acme/critical/utils/FriendsUtils.java
+++
@@ -1,8 +0,0 @@
-package acme.critical.utils;
-
-import java.util.List;
-import java.util.ArrayList;
-
-public class FriendsUtils {
-    public static ArrayList<String> friends = new ArrayList<String>();
-}
