3673f2fef5cfe3e35223146498018fc77ac5a596
IHM <118293336+factrick45@users.noreply.github.com>
2022-12-05 08:03:58 -0600

    pumpkin

--- src/main/java/acme/critical/module/visual/Norender.java
+++ src/main/java/acme/critical/module/visual/Norender.java
@@ -8,14 +8,19 @@ import acme.critical.module.settings.KeybindSetting;
 public class Norender extends Mod {
 	// Allow GUI use while in a nether portal
 	// ClientPlayerEntityMixin
-	private BooleanSetting doPortals = new BooleanSetting("Portals", true);
+	private BooleanSetting portals = new BooleanSetting("Portals", true);
+	private BooleanSetting pumpkin = new BooleanSetting("Pumpkin", true);
 
 	public Norender() {
 		super("Norender", "Disable rendering of certain effects.", Category.VISUAL);
-		addSettings(doPortals, new KeybindSetting("Key", 0));
+		addSettings(portals, pumpkin, new KeybindSetting("Key", 0));
 	}
 
 	public boolean portalsEnabled() {
-		return doPortals.isEnabled();
+		return portals.isEnabled();
+	}
+
+	public boolean pumpkinEnabled() {
+		return pumpkin.isEnabled();
 	}
 }
