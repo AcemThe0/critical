ac05598d0cb709b16b14674becec0d37410b02a1
IHM <118293336+factrick45@users.noreply.github.com>
2023-01-09 20:32:18 -0600

    Left Autoclicker

--- src/main/java/acme/critical/module/misc/Autoclicker.java
+++ src/main/java/acme/critical/module/misc/Autoclicker.java
@@ -2,6 +2,7 @@ package acme.critical.module.misc;
 
 import net.minecraft.client.MinecraftClient;
 
+import acme.critical.mixin.MinecraftClientAccessor;
 import acme.critical.mixinterface.KeyBindingInterface;
 import acme.critical.module.Mod;
 import acme.critical.module.settings.BooleanSetting;
@@ -9,20 +10,34 @@ import acme.critical.module.settings.NumberSetting;
 import acme.critical.module.settings.KeybindSetting;
 
 public class Autoclicker extends Mod {
+	private BooleanSetting lClick = new BooleanSetting("LClick", false);
+	private NumberSetting lDelay = new NumberSetting("LDelay", 1, 60, 40, 1);
 	private BooleanSetting rClick = new BooleanSetting("RClick", true);
 	private NumberSetting rDelay = new NumberSetting("RDelay", 1, 60, 1, 1);
 
+	private short lClickTimer = 0;
 	private short rClickTimer = 0;
 
 	private MinecraftClient mc = MinecraftClient.getInstance();
 
 	public Autoclicker() {
 		super("Autoclicker", "Automatically click mouse buttons.", Category.MISC);
-		addSettings(rClick, rDelay, new KeybindSetting("Key", 0));
+		addSettings(lClick, lDelay, rClick, rDelay, new KeybindSetting("Key", 0));
 	}
 
 	@Override
 	public void onTick() {
+		if (lClick.isEnabled()) {
+			lClickTimer += 1;
+			if (lClickTimer == lDelay.getValueInt()) {
+				mc.options.attackKey.setPressed(true);
+				((MinecraftClientAccessor) (Object) mc).doAttack();
+			}
+			if (lClickTimer > lDelay.getValueInt()) {
+				((KeyBindingInterface) (Object) mc.options.attackKey).release();
+				lClickTimer = 0;
+			}
+		}
 		if (rClick.isEnabled()) {
 			rClickTimer += 1;
 			if (rClickTimer == rDelay.getValueInt()) {
