195da6df830c7633292d77c71162119536dc77eb
IHM <118293336+factrick45@users.noreply.github.com>
2023-01-13 14:42:19 -0600

    autolog players

--- src/main/java/acme/critical/module/misc/Autolog.java
+++ src/main/java/acme/critical/module/misc/Autolog.java
@@ -1,5 +1,6 @@
 package acme.critical.module.misc;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.text.Text;
 
@@ -8,13 +9,15 @@ import acme.critical.module.settings.BooleanSetting;
 import acme.critical.module.settings.NumberSetting;
 import acme.critical.module.settings.KeybindSetting;
 import acme.critical.utils.ChatUtils;
+import acme.critical.utils.FriendsUtils;
 
 public class Autolog extends Mod {
-	public NumberSetting logHealth = new NumberSetting("LogHealth", 0, 20, 15, 1);
+	public NumberSetting logHealth = new NumberSetting("Min Health", 0, 20, 15, 1);
+	public BooleanSetting logPlayer = new BooleanSetting("Players", false);
 
 	public Autolog() {
-		super("Autolog", "Disconnect when conditions are met.", Category.MISC);
-		addSettings(logHealth, new KeybindSetting("Key", 0));
+		super("Autolog", "Log when conditions are met.", Category.MISC);
+		addSettings(logHealth, logPlayer, new KeybindSetting("Key", 0));
 	}
 
 	public void onTick() {
@@ -30,6 +33,15 @@ public class Autolog extends Mod {
 	private String shouldLog() {
 		if (mc.player.getHealth() < logHealth.getValueInt())
 			return "Health < " + logHealth.getValueInt();
+		if (logPlayer.isEnabled()) {
+			for (Entity ent : mc.world.getEntities()) {
+				if (
+					ent != mc.player
+					&& ent.isPlayer()
+					&& !FriendsUtils.isFriend(ent)
+				) return "Player " + ent.getName().getString() + " entered view"; 
+			}
+		}
 		return null;
 	}
 }
