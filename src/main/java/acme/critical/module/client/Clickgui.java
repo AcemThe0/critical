874f4cd52f0f7ba11a88d94d761641bfbbb1e945
IHM <118293336+factrick45@users.noreply.github.com>
2023-07-20 20:52:31 -0500

    Add LoadTheme button

--- src/main/java/acme/critical/module/client/Clickgui.java
+++ src/main/java/acme/critical/module/client/Clickgui.java
@@ -29,31 +29,36 @@ public class Clickgui extends Mod {
     public static ColorSetting colorSelected =
         new ColorSetting("Selected", 0xffffffff);
     public static BooleanSetting flatMode =
-        new BooleanSetting("Flat mode", false);
-    public static BooleanSetting reloadTheme =
-        new BooleanSetting("Save Theme", false);
+        new BooleanSetting("FlatMode", false);
+    public static BooleanSetting saveTheme =
+        new BooleanSetting("SaveTheme", false);
+    public static BooleanSetting loadTheme =
+        new BooleanSetting("LoadTheme", false);
 
     public Clickgui() {
         super("ClickGUI", "Customization!", Category.CLIENT);
         colorBase.setLabeled(true);
-        colorBase.setColor(Render2DUtils.getColors(0)[1]);
         colorTitle.setLabeled(true);
-        colorTitle.setColor(Render2DUtils.getColors(1)[1]);
         colorSelected.setLabeled(true);
-        colorSelected.setColor(Render2DUtils.getColors(2)[1]);
-        flatMode.setEnabled(Render2DUtils.isFlat());
+        updateColors();
         addSettings(
             arraylist, descriptions, chatFeedback, arrows, colorBase,
-            colorTitle, colorSelected, flatMode, reloadTheme
+            colorTitle, colorSelected, flatMode, saveTheme, loadTheme
         );
         Critical.INSTANCE.eventBus.subscribe(this);
     }
 
     @CriticalSubscribe
     public void onTickAlways(EventTick event) {
-        if (!reloadTheme.isEnabled())
+        if (loadTheme.isEnabled()) {
+            loadTheme.setEnabled(false);
+            Profile.INSTANCE.load(ThemeFile.class);
+            updateColors();
+        }
+
+        if (!saveTheme.isEnabled())
             return;
-        reloadTheme.setEnabled(false);
+        saveTheme.setEnabled(false);
 
         ArrayList<int[]> theme = new ArrayList<>();
         theme.add(Render2DUtils.getComplements(colorBase.getColor()));
@@ -63,11 +68,17 @@ public class Clickgui extends Mod {
         Render2DUtils.updateTheme(theme, flatMode.isEnabled());
 
         Profile.INSTANCE.save(ThemeFile.class);
-        Profile.INSTANCE.load(ThemeFile.class);
     }
 
     @Override
     public void onEnable() {
         setEnabled(false);
+    }
+
+    private void updateColors() {
+        colorBase.setColor(Render2DUtils.getColors(0)[1]);
+        colorTitle.setColor(Render2DUtils.getColors(1)[1]);
+        colorSelected.setColor(Render2DUtils.getColors(2)[1]);
+        flatMode.setEnabled(Render2DUtils.isFlat());
     }
 }
